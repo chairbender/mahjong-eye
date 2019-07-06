25c7eb67b1914b8826e454ed9ba4bccd0767664f
Kyle Hipke <kyle.hipke@cirrus10.com>
2019-07-05 22:16:56 -0700

    get basic spring boot + javafx app working displaying the webcam with opencv

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # mahjong-eye
 Using computer vision to enhance the game of mahjong.
+
+# Setup
+1. Install JavaFX SDK.
+2. Add VM options to ensure JavaFX is loaded.
+   ```
+   --module-path="C:\path\to\javafx-sdk-12.0.1\lib" --add-modules javafx.controls,javafx.fxml,javafx.graphics,javafx.web`
+   ```
+3. Download OpenCV for your system and put the correct JAR and dll into this project's folder.
+4. Modify build.gradle to point to your opencv JAR
---
+++ src/main/java/com/chairbender/mahjongeye/MahjongeyeApplication.java
@@ -0,0 +1,66 @@
+package com.chairbender.mahjongeye;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import org.opencv.core.Core;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class MahjongeyeApplication extends Application {
+	private ConfigurableApplicationContext springContext;
+	private Parent rootNode;
+	private FXMLLoader fxmlLoader;
+
+
+	public static void main(String[] args) {
+		// load the native OpenCV library
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		launch(args);
+	}
+
+	@Override
+	public void init() throws Exception {
+		springContext = SpringApplication.run(MahjongeyeApplication.class);
+		fxmlLoader = new FXMLLoader();
+		fxmlLoader.setControllerFactory(springContext::getBean);
+	}
+
+	/**
+	 * The main entry point for all JavaFX applications.
+	 * The start method is called after the init method has returned,
+	 * and after the system is ready for the application to begin running.
+	 *
+	 * <p>
+	 * NOTE: This method is called on the JavaFX Application Thread.
+	 * </p>
+	 *
+	 * @param primaryStage the primary stage for this application, onto which
+	 *                     the application scene can be set.
+	 *                     Applications may create other stages, if needed, but they will not be
+	 *                     primary stages.
+	 * @throws Exception if something goes wrong
+	 */
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		fxmlLoader.setLocation(getClass().getResource("/fxml/main.fxml"));
+		rootNode = fxmlLoader.load();
+
+		primaryStage.setTitle("Hello World");
+		Scene scene = new Scene(rootNode, 800, 600);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+
+	}
+
+	@Override
+	public void stop() {
+		springContext.stop();
+	}
+
+}
